e827f1acb5b2d3e098829840fbf93216322040c5
djmrr <mikedjrussell@gmail.com>
2014-09-13 10:24:12 -0600

    changede routes and pages to lowercase

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/FAQ'
+  get 'static_pages/faq'
 
-  get 'static_pages/Home'
+  get 'static_pages/home'
 
-  get 'static_pages/General_Info'
+  get 'static_pages/general_info'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
