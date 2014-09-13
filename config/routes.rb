cfd3b9c30d5e5b250789c23321b4c37fe68d14d8
Ryan LeViseur <vidkun@vlfservices.com>
2014-09-13 14:53:57 -0600

    fix more devise issues with user registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   devise_scope :user do
     get '/sign_up', to: 'devise/registrations#new', as: :sign_up
     get '/sign_in', to: 'devise/sessions#new', as: :sign_in
