213d14bd7a4fbda877e1872d900639f3fe599c65
Rocio <rsoto1@hotmail.com>
2014-09-13 13:52:21 -0600

    Added Posts and Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,19 @@
 class CommentsController < ApplicationController
-  def index
-  end
-  def edit
-  end
-
-  def new
-  end
-
   def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
   end
-
-  def delete
+ 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
   end
+ 
+  private
+    def comment_params
+      params.require(:comment).permit(:title, :author, :body)
+    end
 end
